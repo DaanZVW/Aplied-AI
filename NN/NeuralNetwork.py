a2733acec5a58b0abc86ecc1bb5246c60c34ab43
Daan Zimmerman van Woesik <43244099+DaanZVW@users.noreply.github.com>
2020-09-29 13:49:32 +0200

    Added perceptron connector

--- NN/NeuralNetwork.py
+++ NN/NeuralNetwork.py
@@ -1,21 +1,31 @@
 import numpy as np
 import copy
 
-class Input_Neuron:
-    def __init__( self ):
-        pass
+def sigmoid( self, x ):
+    return 1 / (1 + np.exp(-x))
 
-class Weighted_Neuron:
-    def __init__( self, neuron, weight ):
-        self.neuron = neuron
-        self.weight = weight
+class Input_Perceptron:
+    def __init__( self, value=0 ):
+        self.value = value
 
-class Neuron:
+    def set_value( self, value ):
+        self.value = value
+
+    def get_value( self ):
+        return self.value
+
+class Perceptron:
     def __init__( self ):
-        self.neuron_childs = []
-        self.neuron_parents = []
+        self.perceptron_childs = []
+        self.perceptron_parents = []
         self.delta = 0
         self.bias = 0
+
+    def add_childs( self, childs ):
+        self.perceptron_childs = childs
+    
+    def add_parents( self, parents ):
+        self.perceptron_parents = parents
 
     def forward_propagation( self ):
         pass
@@ -29,13 +39,33 @@ class Neuron:
     def update_neuron( self ):
         pass
 
-class Neural_network:
-    def __init__(self, n_inputs, n_outputs, conf_neurons):
-        self.neurons = []
-    
+class Neural_Network:
+    def __init__( self, n_inputs, conf_neurons, n_outputs ):
+        self.perceptrons = [[] for _ in range( len(conf_neurons) + 2 )]
+        self.perceptrons[0] = [Input_Perceptron() for _ in range( n_inputs )]
+
+        for index in range(1, len(conf_neurons)+1):
+            self.perceptrons[index] = [Perceptron() for _ in range( conf_neurons[-1] )]
+
+        self.perceptrons[-1] = [Perceptron() for _ in range( n_outputs )]
+
+        self.inputs = self.perceptrons[0]
+
+    def connect_all( self ):
+        for layer_index, layer in enumerate( self.perceptrons[1:] ):
+            for perceptron in layer:
+                # Check if layer is not input layer
+                # Make childeren if true
+                if layer_index != 0:
+                    perceptron.add_childs( self.perceptrons[layer_index+1] )
+
+                # Check if layer is not output layer
+                # Make parents if true
+                if layer_index != len(self.perceptrons)-2:
+                    perceptron.add_parents( self.perceptrons[layer_index-1] )
+
 
 def import_data_from_file( filename ):
-    
     data = np.genfromtxt(
         filename,
         delimiter=',',
@@ -75,5 +105,12 @@ def import_data_from_file( filename ):
 
 
 if __name__ == "__main__":
-    print(  import_data_from_file( "iris.data" )[2] )
+    # print(  import_data_from_file( "iris.data" )[2] )
+
+    john = Neural_Network( 2, [2], 1 )
+    john.connect_all()
     
+    for i in john.perceptrons[1:]:
+        print()
+        for j in i:
+            print( j.name, j.perceptron_childs, j.perceptron_parents )
